8cc1f122386d123347af8f61b677ccef20e188f9
Junwen Feng <stan@vibe.us>
2020-08-03 17:56:34 +0800

    deployt

---
+++ precache-manifest.c65e01e73217be84c9f1110f20f6c433.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ed47ec62bf122f31736f709a48ca5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0152701c223f0a284cc2",
+    "url": "/static/css/main.a3fd96b7.chunk.css"
+  },
+  {
+    "revision": "0a0542d1f0b780fa4867",
+    "url": "/static/js/2.c1ce07a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c1ce07a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0152701c223f0a284cc2",
+    "url": "/static/js/main.22aa48d0.chunk.js"
+  },
+  {
+    "revision": "06fac14e689b998b04a8",
+    "url": "/static/js/runtime-main.c68afe41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
